5348438c3092d40b5e814d45196d6d3e55bd4c04
Shriram Chandak <shriram.chandak@tavant.com>
2019-06-18 16:21:10 +0530

    added pug and handlebar templates

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
-const rootDir = require('./util/path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+//uncomment for handlebar template
+
+const expressHbs = require('express-handlebars');
+
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts/',
+    defaultLayout: 'main-layout',
+    extname: 'hbs',
+  })
+);
+app.set('view engine', 'hbs');
+
+// uncomment for pug template
+
+//app.set('view engine', 'pug');
+
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(rootDir, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(rootDir, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server started at port ${port}`);
-});
+app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,18 +1,35 @@
 const express = require('express');
-const path = require('path');
-const rootDir = require('../util/path');
 
 const router = express.Router();
 
-// router /admin/add-product GET
+const products = [];
+
+// uncomment for pug template
+// /admin/add-product => GET
+// router.get('/add-product', (req, res, next) => {
+//   res.render('add-product', {
+//     pageTitle: 'Add Product',
+//     path: '/admin/add-product',
+//   });
+// });
+
+//uncomment for handle bar template
+
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true,
+  });
 });
 
-// router /admin/add-product POST
+// /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
